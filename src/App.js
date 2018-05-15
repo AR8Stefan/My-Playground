97e9ef9fa81b31112fb8f64f8c1ec7b452541477
AR8Stefan <stefan.piatkov@gmail.com>
2018-05-15 16:33:50 -0500

    getting started and learning 'state'

--- src/App.js
+++ src/App.js
@@ -1,19 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import GuestList from './components/GuestList';
 
 class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Stefan1',
+        isConfirmed: true
+      },
+      {
+        name: 'Stefan2',
+        isConfirmed: false
+      },
+      {
+        name: 'Stefan3',
+        isConfirmed: false
+      }
+    ]
+  }
+ // will toggle confirmation 'at' the specified 'index'
+  toggleConfirmationAt = indexToChange => 
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if(index === indexToChange) {
+          return {
+            ...guest,
+            isConfirmed: !guest.isConfirmed
+          };
+        }
+        return guest;
+      })
+    });
+
+  getTotalInvited = () => this.state.guests.length;
+  // getAttending = () =>
+  // getUnconfirmed = () =>
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <div class="App">
+      <header>
+        <h1>RSVP</h1>
+        <p>A Treehouse App</p>
+        <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
+      </header>
+      <div class="main">
+        <div>
+          <h2>Invitees</h2>
+          <label>
+            <input type="checkbox" /> Hide those who haven't responded
+          </label>
+        </div>
+        <table class="counter">
+          <tbody>
+            <tr>
+              <td>Attending:</td>
+              <td>2</td>
+            </tr>
+            <tr>
+              <td>Unconfirmed:</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <GuestList 
+          guests={this.state.guests}
+          toggleConfirmationAt={this.toggleConfirmationAt}
+        />
+
       </div>
+    </div>
     );
   }
 }
